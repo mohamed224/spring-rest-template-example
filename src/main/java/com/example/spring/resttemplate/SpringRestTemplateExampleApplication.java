2926d94aa31220beafbb0cfb265cd32ed9343727
Mohamed DIABY <mdiaby@spark.local>
2020-02-24 15:22:04 +0100

    first commit

---
+++ src/main/java/com/example/spring/resttemplate/SpringRestTemplateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTemplateExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestTemplateExampleApplication.class, args);
+    }
+
+}
