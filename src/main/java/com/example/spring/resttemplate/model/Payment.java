62a22430a8414543e661dfaf766f443ac369664e
Mohamed DIABY <mdiaby@spark.local>
2020-02-25 09:49:09 +0100

    models implemented

--- src/main/java/com/example/spring/resttemplate/model/Payment.java
+++ src/main/java/com/example/spring/resttemplate/model/Payment.java
@@ -1,4 +1,24 @@
 package com.example.spring.resttemplate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private Long orderId;
+    private Double amount;
+    private Long cardNumber;
 }
